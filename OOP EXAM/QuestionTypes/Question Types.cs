72302c3bc0987cea1f7ce5d716c99d195a0b1790
FYM3 <yousefmohame8i8@gmail.com>
2026-09-17 04:15:58 +0300

    Q3) => Question Types

--- OOP EXAM/QuestionTypes/Question Types.cs
+++ OOP EXAM/QuestionTypes/Question Types.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Examtype
+namespace Questiontypes
 {
-    public enum ExamType { Final, Practical }
+    public enum QuestionTypes { TrueorFalse, MCQ }
 }
