d56d0da9c248b47a8c547fdd3c5b7348f693a9a7
FYM3 <yousefmohame8i8@gmail.com>
2026-09-17 04:31:21 +0300

    Q4 => class answers

---
+++ OOP EXAM/Answers/Answers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_EXAM.answers
+{
+    internal class Answers
+    {
+		private int _AnswerId;
+		private string _AnswerText = string.Empty;
+
+        public int AnswerId
+        {
+            get { return _AnswerId; }
+            set
+            {
+                if (value >= 0)
+                    _AnswerId = value;
+            }
+        }
+        public string AnswerText
+        {
+			get { return _AnswerText; }
+			set
+			{
+				if (!string.IsNullOrEmpty(value))
+					_AnswerText = value;
+			}
+		}
+
+        public Answers(int answerId, string answerText)
+        {
+            AnswerId = answerId;
+            AnswerText = answerText;
+        }
+
+
+
+    }
+}
