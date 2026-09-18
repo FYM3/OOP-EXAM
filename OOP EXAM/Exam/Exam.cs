ca48a97d3ad5d98d3e7062fef5a717fceb3a9f0f
FYM3 <yousefmohame8i8@gmail.com>
2026-09-18 05:28:46 +0300

    Q6) => Class Exam

---
+++ OOP EXAM/Exam/Exam.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_EXAM.exam
+{
+    internal class Exam
+    {
+		private int _Timeofexam;
+        private int _NumberofQuestions;
+
+        public int Timeofexam
+        {
+			get { return _Timeofexam; }
+			set
+			{
+				if (value >= 30 && value <= 180)
+					_Timeofexam = value;
+			}
+		}
+		
+		public int NumberofQuestions
+        {
+			get { return _NumberofQuestions; }
+			set
+			{
+				if (value > 0)
+					_NumberofQuestions = value;
+			}
+		}
+
+
+        public Exam(int timeofexam, int numberofQuestions)
+        {
+            Timeofexam = timeofexam;
+            NumberofQuestions = numberofQuestions;
+        }
+
+
+		public virtual void ShowExam()
+		{
+
+		}
+    }
+}
---
+++ OOP EXAM/Practical Exam/PracticalExam.cs
@@ -0,0 +1,20 @@
+﻿using OOP_EXAM.exam;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_EXAM.practical_Exam
+{
+    internal class PracticalExam : Exam
+    {
+        public PracticalExam(int timeofexam, int numberofQuestions) : base(timeofexam, numberofQuestions)
+        {
+        }
+
+
+        public override void ShowExam()
+        {
+            // Not implementated yet
+        }
+    }
+}
