af086d5f3b58071156c609754dc862ac1c9df0ce
FYM3 <yousefmohame8i8@gmail.com>
2026-09-18 06:35:39 +0300

    Q9) => Final Exam

--- OOP EXAM/Final Exam/FinalExam.cs
+++ OOP EXAM/Final Exam/FinalExam.cs
@@ -14,7 +14,19 @@ namespace OOP_EXAM.final_Exam
 
         public override void ShowExam()
         {
-            // Not implementated yet
+            Console.WriteLine("Final Exam Results:\n");
+
+            for (int i = 0; i < Questions.Length; i++)
+            {
+                Console.WriteLine($"Question {i + 1}: {Questions[i].Body}");
+                Console.WriteLine("Answers:");
+                for (int j = 0; j < Questions[i].AnswerList.Length; j++)
+
+                    Console.WriteLine($"{Questions[i].AnswerList[j].AnswerId}. " + $"{Questions[i].AnswerList[j].AnswerText}");
+
+                Console.WriteLine($"Grade: {Questions[i].Mark}\n");
+            }
         }
+
     }
 }
